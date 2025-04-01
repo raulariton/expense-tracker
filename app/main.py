faa22fd8d2f01734d58e14f402a78f9e2b90f2d1
stefars <hidrostefan@gmail.com>
2025-04-01 14:42:08 +0300

    structure

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import imutils
 from imutils.perspective import four_point_transform
-import cv2
 import pytesseract
 from image_processing import *
 from llm_text_extraction import LLM_extraction
@@ -8,11 +7,6 @@ from llm_text_extraction import LLM_extraction
 
 
 def zoom_in(img_cpy,bounding_box,ratio):
-
-    # some functions to zoom in on the image
-    epsilon = 0.05 * cv2.arcLength(bounding_box, True)  # You can adjust this parameter for more/less approximation
-    approx = cv2.approxPolyDP(bounding_box, epsilon, True)
-
 
     receipt = four_point_transform(img_cpy, bounding_box * ratio)
     cv2.imshow("Receipt", receipt)
@@ -23,12 +17,9 @@ def recognize_text(receipt):
     receipt = pre_process_text(receipt)
 
     options = "--psm 4"
-    text = pytesseract.image_to_string(
-        cv2.cvtColor(receipt, cv2.COLOR_BGR2RGB),
-        config=options)
 
-    cv2.imshow("Text",receipt)
-    # show the raw output of the OCR process
+    text = pytesseract.image_to_string(receipt, config=options)
+
     print("[INFO] raw output:")
     print("==================")
     print(text)
@@ -36,8 +27,13 @@ def recognize_text(receipt):
     return text
 
 def extract_receipt(orig):
+    '''
 
-    image = imutils.resize(orig.copy(), width=500)  # Resise image to 500 width
+    :param orig:
+    :return the image zoomed in on the receipt (not always):
+    '''
+
+    image = imutils.resize(orig.copy(), width=500)
 
     edged,ratio = pre_process_image(image.copy())
     bounding_box = extract_receipt_bounding_box(edged, image.copy())
@@ -52,19 +48,10 @@ def extract_receipt(orig):
 
 def main():
 
-    #take the iamge
-
     receipt = extract_receipt(ORIGINAL_IMAGE)
     text = recognize_text(receipt)
     LLM_extraction(text)
     cv2.waitKey(0)
-
-
-
-
-
-
-
 
 
 
