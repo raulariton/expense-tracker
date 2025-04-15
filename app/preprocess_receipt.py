1eed6ad7d497f3ec979b95f28395b846017de372
raul-it0o0 <ariton.raul@gmail.com>
2025-04-15 13:13:19 +0300

    Add segmentation and preprocessing logic

---
+++ app/preprocess_receipt.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+from matplotlib import cm
+from skimage.transform import radon
+from PIL import Image
+from numpy import asarray, mean, array
+
+try:
+    from parabolic import parabolic
+
+    def argmax(x):
+        return parabolic(x, np.argmax(x))[0]
+
+except ImportError:
+    from numpy import argmax
+
+
+def preprocess_receipt(image):
+    """
+    Pre-processes the image to prepare it for OCR.
+    """
+
+    # convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # apply CLAHE histogram equalization
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    gray = clahe.apply(gray)
+
+    # denoise the image
+    denoised = cv2.fastNlMeansDenoising(
+        src=gray, dst=None, h=5, templateWindowSize=7, searchWindowSize=21
+    )
+
+    # apply adaptive thresholding (binarization)
+    thresh = cv2.adaptiveThreshold(
+        denoised, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 11
+    )
+
+    # rotate the image
+    rotated = rotate_image(thresh)
+
+
+def rotate_image(numpy_image):
+    def rms_flat(a):
+        """
+        Return the root-mean-square of all the elements of *a*, flattened out.
+        """
+        return np.sqrt(np.mean(np.abs(a) ** 2))
+
+    # convert image to PIL (pillow) format
+    image = Image.fromarray(numpy_image.astype("uint8"))
+
+    # convert to grayscale
+    image = image.convert("L")
+    # demean - make the brightness extend above and below zero
+    image = image - mean(image)
+
+    # Do the radon transform and display the result
+    sinogram = radon(image)
+
+    # Find the RMS value of each row and find "busiest" rotation,
+    # where the transform is lined up perfectly with the alternating dark
+    # text and white lines
+    r = array([rms_flat(line) for line in sinogram.transpose()])
+    rotation = argmax(r)
+
+    # convert image to PIL (pillow) format
+    image = Image.fromarray(numpy_image.astype("uint8"))
+    # Rotate the image by the inverse of the output
+    # rotate as less as possible
+    if (90 - rotation) < rotation:
+        rotation = 90 - rotation
+
+    image = image.rotate(rotation, expand=True, fillcolor="white")
+
+    # show image
+    image.show()
+
+    # return as numpy array
+    return asarray(image)
---
+++ main.py
@@ -0,0 +1,38 @@
+import cv2
+from app.detect_receipt import detect_receipt
+from app.preprocess_receipt import preprocess_receipt
+
+
+# TODO:
+# make sure to remove any DEBUG comments
+# these are just made for visualization purposes
+
+
+def main():
+
+    # 1. Receive image
+    # INSERT IMAGE PATH HERE (e.g., "path/to/image.jpg")
+    image_path = ""
+    image = cv2.imread(image_path)
+    if image is None:
+        return
+
+    # 2. Detect receipt, crop it and perspective transform it
+    receipt = detect_receipt(image)
+
+    # 3. Preprocess the image for OCR
+    preprocessed_receipt = preprocess_receipt(receipt)
+    return  # the rest of the function is work in progress
+
+    # 4. Perform OCR
+    # ocr_text = pytesseract.image_to_string(preprocessed_receipt)
+
+    # 5. Extract text using LLM
+    # output_json = LLM_extraction(ocr_text)
+
+    # print the output JSON
+    # print(output_json)
+
+
+if __name__ == "__main__":
+    main()
