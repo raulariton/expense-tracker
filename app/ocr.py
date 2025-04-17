ae65115c14c2e4d6a02d88540c8f9f0ebf767819
raul-it0o0 <ariton.raul@gmail.com>
2025-04-18 00:55:11 +0300

    Clean code on ocr.py

--- app/ocr.py
+++ app/ocr.py
@@ -8,7 +8,8 @@ import re
 import numpy as np
 from ultralytics import YOLO
 
-
+EASYOCR = 0
+TESSERACT = 1
 
 
 def inference(file):
@@ -30,14 +31,14 @@ def inference(file):
         return cropped
 
 
-
-def zoom_in(img_cpy,bounding_box,ratio):
+def zoom_in(img_cpy, bounding_box, ratio):
 
     receipt = four_point_transform(img_cpy, bounding_box * ratio)
     cv2.imshow("Receipt", receipt)
     return receipt
 
-def recognize_text_tess(receipt):
+
+def tesseract_ocr(receipt):
 
     options = "--oem 3 --psm 4"
 
@@ -48,35 +49,45 @@ def recognize_text_tess(receipt):
     print(text)
     print("\n")
 
-    bounding_boxes(receipt)
+    # DEBUG
+    draw_tesseract_bounding_boxes(receipt)
 
     return text
 
-def bounding_boxes(receipt):
+# DEBUG
+#  for visualization purposes (understanding the ocr)
+def draw_tesseract_bounding_boxes(receipt):
     """
-    Draws text bounding boxes for tesseract.
-    :param receipt: cv2 image
-    :return:
+    Draws text bounding boxes for Tesseract.
+    :param receipt: Receipt image in cv2 (numpy array) format
     """
+
     data = pytesseract.image_to_data(receipt, output_type=pytesseract.Output.DICT)
 
     # Iterate through the data and print the text and its bounding box
-    for i in range(len(data['text'])):
-        text = data['text'][i]
+    for i in range(len(data["text"])):
+        text = data["text"][i]
         if text.strip():  # Ensure the text is not empty
-            x, y, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+            x, y, w, h = (
+                data["left"][i],
+                data["top"][i],
+                data["width"][i],
+                data["height"][i],
+            )
             cv2.rectangle(receipt, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-def recognize_text_easy(receipt):
+
+def easy_ocr(receipt):
     """
-    Uses easyOCR to recognize and draw bounding boxes.
+    Uses EasyOCR to recognize and draw bounding boxes.
     :param receipt: cv2 image
     :return: list of strings (recognized text)
     """
 
-    reader = easyocr.Reader(['ro'], gpu=False)
-    data = reader.readtext(receipt,decoder='beamsearch',link_threshold=0.8)
-    print(data)
+    reader = easyocr.Reader(["ro"], gpu=False)
+    data = reader.readtext(receipt, decoder="beamsearch", link_threshold=0.8)
+    # DEBUG
+    # print(data)
     text_data = []
 
     for bbox, text, score in data:
@@ -86,26 +97,25 @@ def recognize_text_easy(receipt):
 
     return text_data
 
-def extract_text(receipt,mode):
-    """
 
-    :param receipt: cv2 image
-    :param mode: 0 - uses easyOCR, 1 - uses tesseract
-    :return: string: Extracted amount.
+def extract_text(receipt, mode):
+    """
+    Extracts text from the receipt using OCR.
+    :param receipt: The receipt image in cv2 (numpy array) format.
+    :param mode: The OCR engine to use (use constant EASYOCR or TESSERACT).
+    :return: Extracted and processed string.
     """
 
     text = ""
-    if mode == 0:
-        text = recognize_text_easy(receipt)
-    elif mode == 1:
-        text = recognize_text_tess(receipt)
+    if mode == EASYOCR:
+        text = easy_ocr(receipt)
+    elif mode == TESSERACT:
+        text = tesseract_ocr(receipt)
+
+    return process_text(text)
 
 
-    print(text)
-
-    return find_total(text)
-
-def find_total(text):
+def process_text(text):
     """
     Finds the first list element containing total (but not TVA), using regex.
     Assumes amount is at position to the right from total found.
@@ -113,17 +123,12 @@ def find_total(text):
     :return: the amount
     """
 
-    pattern = r'^(?!.*TVA).*total.*$'
-    amount = r'\d[.,]\d{2}'
+    pattern = r"^(?!.*TVA).*total.*$"
+    amount = r"\d[.,]\d{2}"
 
     for i in range(0, len(text)):
         if re.search(pattern, text[i], re.IGNORECASE):
-            modified_text = text[i+1].replace(" ", "")
-            if re.search(amount,modified_text,re.IGNORECASE):
-                print("Total price is: " + text[i+1])
+            modified_text = text[i + 1].replace(" ", "")
+            if re.search(amount, modified_text, re.IGNORECASE):
                 return modified_text
-            else:
-                print("Total price is: "+text[i+1]+"."+text[i+2])
-            return modified_text+"."+text[i+2]
-
-
+            return modified_text + "." + text[i + 2]
