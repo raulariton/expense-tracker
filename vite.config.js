5cb5d74674138511482a24cd2de387c93f29e35b
raul-it0o0 <ariton.raul@gmail.com>
2025-04-29 14:25:46 +0300

    Modify Vite config for debugging

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      keepNames: true,
+    },
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
 })
