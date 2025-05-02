78955bf0d82abe89d3af076e382bd321d1fc53ae
raul-it0o0 <ariton.raul@gmail.com>
2025-05-02 03:14:17 +0300

    Initial authentication commit

---
+++ app/services/auth/login.py
@@ -0,0 +1,27 @@
+from app.services.auth.utils import db_dependency, bcrypt_context
+import app.models.dbmodels as models
+
+
+# util function
+def authenticate_user(email: str, password: str,
+                      db: db_dependency):
+    """
+    Authenticates the user (happens during login) by
+    checking if the given email and password match.
+    """
+    # find user in db
+    user = db.query(models.User).filter(models.User.email == email).first()
+
+    # check if user exists
+    if not user:
+        return False
+
+    # user exists
+
+    # check if password is correct (compare hashed password)
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+
+    # password is correct
+    # return user (db entry, pydantic model)
+    return user
---
+++ app/services/auth/register.py
@@ -0,0 +1,45 @@
+import app.models.dbmodels as models
+from pydantic import BaseModel
+from app.services.auth.utils import db_dependency, bcrypt_context
+
+class UserCreationRequest(BaseModel):
+    email: str
+    password: str
+
+async def create_user(
+        db: db_dependency,
+        user_creation_request: UserCreationRequest):
+    """
+    Registers a new user, adding them to the database.
+    :return: The created user
+    """
+
+    new_user_model = models.User(
+        email=user_creation_request.email,
+        hashed_password=bcrypt_context.hash(user_creation_request.password)
+    )
+
+    db.add(new_user_model)
+    db.commit()
+
+    # update User object with the generated id
+    db.refresh(new_user_model)
+
+    # return the user (db entry, pydantic model)
+    return new_user_model
+
+def get_user_by_email(email: str, db: db_dependency):
+    """
+    Searches for a user in the database by email.
+    """
+    # find user
+    user = db.query(models.User).filter(models.User.email == email).first()
+
+    # check if user exists
+    if not user:
+        return False
+    # user exists
+
+    # return the user (db entry, pydantic model)
+    return user
+
