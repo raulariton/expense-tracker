4e94001fbdeae0674e3a8c88ebc3d2d6b4a76f14
raul-it0o0 <ariton.raul@gmail.com>
2025-05-03 14:33:57 +0300

    Implement dashboard total summary and category totals summary

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 import SummaryCard from "../components/SummaryCard";
 import CategoryCard from "../components/CategoryCard";
@@ -16,19 +16,28 @@ import {
 } from "react-icons/fa";
 
 import "../styles/dashboard.css";
+import axios from "axios";
 
-const iconMap = {
-  utensils: <FaUtensils />,
-  shopping: <FaShoppingBag />,
-  transport: <FaCar />,
-  invoice: <FaFileInvoice />,
-  tag: <FaTag />,
+const category_and_icon = {
+  "Food & Dining": <FaUtensils />,
+  "Shopping": <FaShoppingBag />,
+  "Transport": <FaCar />,
+  "Bills": <FaFileInvoice />,
+  "Other": <FaTag />,
 };
 
 const Dashboard = () => {
   const navigate = useNavigate();
 
   const isAuthenticated = useContext(AuthContext);
+  const [summary, setSummary] = useState(null);
+  const [categoryTotals, setCategoryTotals] = useState([
+    { category: "Food & Dining", total: 0 },
+    { category: "Shopping", total: 0 },
+    { category: "Transport", total: 0 },
+    { category: "Bills", total: 0 },
+    { category: "Other", total: 0 },
+  ]);
 
   if (!isAuthenticated) {
     // TODO: Redirect to login page, and then back to dashboard
@@ -41,35 +50,87 @@ const Dashboard = () => {
       </MainLayout>
     );
   }
-  const navigate = useNavigate();
 
-const goToAddExpense = () => {
-  navigate("/add-expense");
-};
+  // get summary
+  useEffect( () => {
+    const getSummary = async () => {
+      const token = localStorage.getItem("access_token");
 
+      // submit request with token
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/expenses/total-summary",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
 
-  const userExpenses = db.expenses.filter((e) => e.user_id === currentUser.id);
+        setSummary(response.data);
+      } catch (error) {
+        console.log(error);
+        // Display error message on frontend
+        alert(error);
+      }
+    }
 
-  const todayDate = new Date().toISOString().split("T")[0];
+    getSummary();
+  }, []);
 
-  const summary = {
-    total: userExpenses.reduce((acc, e) => acc + e.amount, 0).toFixed(2),
-    today: userExpenses
-      .filter((e) => e.date.startsWith(todayDate))
-      .reduce((acc, e) => acc + e.amount, 0)
-      .toFixed(2),
-    week: userExpenses
-      .filter((e) => {
-        const date = new Date(e.date);
-        const today = new Date();
-        const oneWeekAgo = new Date();
-        oneWeekAgo.setDate(today.getDate() - 7);
-        return date >= oneWeekAgo && date <= today;
-      })
-      .reduce((acc, e) => acc + e.amount, 0)
-      .toFixed(2)
-  };
+  // get total expenses by category
+  useEffect( () => {
+    const getCategoryTotals = async () => {
+      const token = localStorage.getItem("access_token");
 
+      // submit request with token
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/expenses/category-summary",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+
+        // setCategoryTotals(response.data.category_summary);
+        setCategoryTotals((prevTotals) =>
+          prevTotals.map((category) => {
+            const updatedCategory = response.data.category_summary.find(
+              (item) => item.category === category.category
+            );
+            return updatedCategory ? updatedCategory : category;
+          })
+        );
+      } catch (error) {
+        // Display error message on frontend
+        alert(error);
+      }
+    }
+
+    getCategoryTotals();
+  }, []);
+
+  // const summary = {
+  //   total: userExpenses.reduce((acc, e) => acc + e.amount, 0).toFixed(2),
+  //   today: userExpenses
+  //     .filter((e) => e.date.startsWith(todayDate))
+  //     .reduce((acc, e) => acc + e.amount, 0)
+  //     .toFixed(2),
+  //   week: userExpenses
+  //     .filter((e) => {
+  //       const date = new Date(e.date);
+  //       const today = new Date();
+  //       const oneWeekAgo = new Date();
+  //       oneWeekAgo.setDate(today.getDate() - 7);
+  //       return date >= oneWeekAgo && date <= today;
+  //     })
+  //     .reduce((acc, e) => acc + e.amount, 0)
+  //     .toFixed(2),
+  // };
+
+  /*
   const recent = userExpenses
     .slice(-5)
     .reverse()
@@ -77,7 +138,7 @@ const goToAddExpense = () => {
       name: e.description,
       amount: e.amount,
       time: new Date(e.date).toLocaleString(),
-      icon: db.categories.find((c) => c.id === e.category_id)?.icon || "tag"
+      icon: db.categories.find((c) => c.id === e.category_id)?.icon || "tag",
     }));
 
   const categoryTotals = db.categories.map((cat) => {
@@ -87,6 +148,7 @@ const goToAddExpense = () => {
       .toFixed(2);
     return { title: cat.name, amount: total, icon: cat.icon };
   });
+   */
 
   return (
     <MainLayout>
@@ -95,59 +157,59 @@ const goToAddExpense = () => {
         <div className="summary-cards">
           <SummaryCard
             title="Total Spent"
-            amount={`$${summary.total}`}
+            amount={`$${(summary?.total_month || 0).toFixed(2)}`}
             subtitle="This month"
             icon={<FaCalendarAlt />}
           />
           <SummaryCard
             title="Spent Today"
-            amount={`$${summary.today}`}
+            amount={`$${(summary?.total_day || 0).toFixed(2)}`}
             subtitle="Today"
             icon={<FaCalendarAlt />}
           />
           <SummaryCard
             title="This Week"
-            amount={`$${summary.week}`}
+            amount={`$${(summary?.total_week || 0).toFixed(2)}`}
             subtitle="Last 7 Days"
             icon={<FaCalendarAlt />}
           />
         </div>
 
-        {/* Action Buttons */}
-        <div className="dashboard-actions">
-          <button className="btn-primary" onClick={goToAddExpense}>
-            <FaCamera className="btn-icon" />
-            Add Expense
-          </button>
-        </div>
+        {/*/!* Action Buttons *!/*/}
+        {/*<div className="dashboard-actions">*/}
+        {/*  <button className="btn-primary" onClick={goToAddExpense}>*/}
+        {/*    <FaCamera className="btn-icon" />*/}
+        {/*    Add Expense*/}
+        {/*  </button>*/}
+        {/*</div>*/}
 
         {/* Categories */}
         <div className="category-cards">
-          {categoryTotals.map((cat, i) => (
+          {categoryTotals && categoryTotals.map((categoryTotal, index) => (
             <CategoryCard
-              key={i}
-              title={cat.title}
-              amount={`$${cat.amount}`}
-              icon={iconMap[cat.icon]}
+              key={index}
+              title={categoryTotal.category}
+              amount={`$${(categoryTotal.total.toFixed(2))}`}
+              icon={category_and_icon[categoryTotal.category]}
             />
           ))}
         </div>
 
         {/* Recent Activity */}
-        <div className="activity-card">
-          <h3>Recent Activity</h3>
-          <div className="activity-list">
-            {recent.map((item, i) => (
-              <ActivityItem
-                key={i}
-                icon={iconMap[item.icon]}
-                name={item.name}
-                time={item.time}
-                amount={`-$${item.amount}`}
-              />
-            ))}
-          </div>
-        </div>
+        {/*<div className="activity-card">*/}
+        {/*  <h3>Recent Activity</h3>*/}
+        {/*  <div className="activity-list">*/}
+        {/*    {recent.map((item, i) => (*/}
+        {/*      <ActivityItem*/}
+        {/*        key={i}*/}
+        {/*        icon={iconMap[item.icon]}*/}
+        {/*        name={item.name}*/}
+        {/*        time={item.time}*/}
+        {/*        amount={`-$${item.amount}`}*/}
+        {/*      />*/}
+        {/*    ))}*/}
+        {/*  </div>*/}
+        {/*</div>*/}
       </div>
     </MainLayout>
   );
