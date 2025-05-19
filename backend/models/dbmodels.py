1c5745437b5b990f5988a23e5fb0901e9a8dbf9b
stefars <hidrostefan@gmail.com>
2025-05-19 16:34:20 +0300

    Database updated with Roles Table

--- backend/models/dbmodels.py
+++ backend/models/dbmodels.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Enum, DateTime, Float
+from sqlalchemy.orm import relationship
 from db.database import Base
 import enum
 import uuid
@@ -17,6 +18,16 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    role_id = Column(Integer, ForeignKey("roles.id"), nullable=False, default=1)
+
+    role = relationship("Role",back_populates="users")
+
+class Role(Base):
+    __tablename__ = "roles"
+    id = Column(Integer, primary_key=True)
+    role = Column(String, unique=True)
+
+    users = relationship("User", back_populates="role")
 
 class Expense(Base):
     __tablename__ = "expenses"
