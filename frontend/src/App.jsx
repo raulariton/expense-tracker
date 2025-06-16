ea10453488a53c936eb2110df254cf6bfca73bfd
raul-it0o0 <ariton.raul@gmail.com>
2025-06-17 00:53:51 +0300

    Add page title and favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,11 @@ import AppRoutes from "./routes/AppRoutes";
 import "./styles/global.css";
 
 function App() {
+
+  // TODO: adjust by language
+  document.title =
+    "ExpenseTracker | Your personal finance, made easy"
+
   return (
     <BrowserRouter>
       <AppRoutes/>
